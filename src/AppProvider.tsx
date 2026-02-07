c33095563b3f1dded31215460d3c5ce0e7524c48
Himanshu wadhwa <himanshuhw999@gmail.com>
2026-02-08 04:36:17 +0530

    fix: added react-query for query management and inflight req canceling

--- src/AppProvider.tsx
+++ src/AppProvider.tsx
@@ -1,8 +1,13 @@
 import type { ReactNode } from "react";
 import { AuthProvider } from "./context/AuthContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 export const AppProvider = ({ children }: { children: ReactNode }) => (
-  <AuthProvider>
-    {children}
-  </AuthProvider>
+  <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      {children}
+    </AuthProvider>
+  </QueryClientProvider>
 );
--- src/context/AuthContext.tsx
+++ src/context/AuthContext.tsx
@@ -1,4 +1,6 @@
-import { createContext, useState, type ReactNode } from "react";
+import { QueryClient, useQueryClient } from "@tanstack/react-query";
+import { createContext, useCallback, useMemo, useState, type ReactNode } from "react";
+import { tokenStore } from "../api/axios";
 
 type User = {
     userId: string;
@@ -11,24 +13,41 @@ interface AuthContextValue {
     user :User| null,
     accessToken: string | null,
     setAccessToken: React.Dispatch<React.SetStateAction<string | null>>,
-    setUser : React.Dispatch<React.SetStateAction<User|null>>
+    setUser : React.Dispatch<React.SetStateAction<User|null>>,
+    logout: ()=>void,
 }
 
 export const AuthContext = createContext<AuthContextValue | undefined>(undefined)
 
-export const AuthProvider = ({children}: {children:ReactNode})=>{
-    const [user, setUser] = useState<User | null>(null)
+export const AuthProvider = ({ children }: {children:ReactNode}) => {
+    const [user, setUser] = useState<User | null>(null);
     const [accessToken, setAccessToken] = useState<string | null>(null);
-    const values = {
+    const queryClient = useQueryClient();
+
+    const logout = useCallback(() => {
+        setAccessToken(null);
+        setUser(null);
+        
+        queryClient.cancelQueries();
+        queryClient.clear();
+
+        tokenStore.set(null);
+    },[queryClient]);
+
+    const value = useMemo(
+        () => ({
         user,
         accessToken,
         setUser,
-        setAccessToken
-    }
-        
-    return (
-    <AuthContext.Provider value={values}>
-        {children}
+        setAccessToken,
+        logout,
+    }),
+    [user, accessToken]
+  );
+
+  return (
+    <AuthContext.Provider value={value}>
+      {children}
     </AuthContext.Provider>
-    )
-}
+  );
+};
